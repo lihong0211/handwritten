82f447b14508cd6139de8bb6c3e79b60915f2cc2
黎洪 <lihong@bilibili.com>
2022-02-24 18:16:51 +0800

    feat: jsonp

--- promise/test1.js
+++ promise/test1.js
@@ -15,7 +15,7 @@ class Promise {
       this.reject(e)
     }
   }
-
+  
   resolve (value) {
     if (this.state === PENDING_STATE) {
       this.value = value
@@ -46,8 +46,8 @@ class Promise {
   }
 
   processor (promise, x, resolve, reject) {
-    if (promise == x) {
-      reject(new TypeError('Chaining cycle detected for promise'));
+    if (promise === x) {
+      reject(new TypeError('equal'))
     }
     if (isObject(x) || isFunction(x)) {
       let called = false
@@ -72,7 +72,7 @@ class Promise {
           called = true
           resolve(x)
         }
-      } catch (e) {
+      } catch(e) {
         if (called) return
         called = true
         reject(e)
@@ -106,7 +106,6 @@ class Promise {
           }
         })
       }
-
       if (this.state === FULFILLED_STATE) {
         wrapOnFulfilled()
       } else if (this.state === REJECTED_STATE) {
@@ -117,135 +116,6 @@ class Promise {
       }
     })
     return promise
-  }
-
-  catch (cb) {
-    return this.then(null, cb)
-  }
-
-  finally (cb) {
-    return this.then(
-      res => {
-        cb()
-        return res
-      },
-      reason => {
-        cb()
-        throw reason
-      }
-    )
-  }
-
-  static resolve (val) {
-    return val instanceof Promise
-      ? val
-      : new Promise(resolve => {
-        resolve(val)
-      })
-  }
-
-  static reject (reason) {
-    return new Promise((resolve, reject) => {
-      reject(reason)
-    })
-  }
-  
-  static all (promises) {
-    return new Promise((resolve, reject) => {
-      const res = []
-      const len = promises.length
-      let resolved = 0
-
-      for (let i = 0; i < len; i++) {
-        promises[i].then(
-          val => {
-            res.push(val)
-            if (++resolved === len) {
-              resolve(res)
-            }
-          },
-          reason => {
-            reject(reason)
-          }
-        )
-      }
-    })
-  }
-
-  // 当任何一个被传入的 promise 成功的时候, 
-  // 无论其他的 promises 成功还是失败，此函数会将那个成功的 promise 作为返回值 
-  // 如果所有传入的 promises 都失败, Promise.any 将返回异步失败，和一个 AggregateError 对象，
-  // 它继承自 Error，有一个 error 属性，属性值是由所有失败值填充的数组。
-
-  static any (promises) {
-    return new Promise((resolve, reject) => {
-      const len = promises.length
-      let rejected = 0
-      let res = []
-      for (let i = 0; i < len; i++) {
-        promises[i].then(
-          val => {
-            resolve(val)
-          },
-          reason => {
-            res.push(reason)
-            if (++rejected === len) {
-              // ...
-              reject(new Error(res))
-            }
-          }
-        )
-      }
-    })
-  }
-
-  // 一旦迭代器中的某个promise解决或拒绝，返回的 promise就会解决或拒绝。
-  static race (promises) {
-    return new Promise((resolve, reject) => {
-      const len = promises.length
-      for (let i = 0; i < len; i++) {
-        promises[i].then(
-          val => {
-            resolve(val)
-          },
-          reason => {
-            reject(reason)
-          }
-        )
-      }
-    })
-  }
-
-  // 方法返回一个在所有给定的promise都已经fulfilled或rejected后的promise，
-  // 并带有一个对象数组，每个对象表示对应的promise结果。status--value--reason
-  static allSettled (promises) {
-    return new Promise((resolve, reject) => {
-      const len = promises.length
-      let resolved = 0
-      const res = []
-      for (let i = 0; i < len; i++) {
-        promises[i].then(
-          value => {
-            res[i] = {
-              status: FULFILLED_STATE,
-              value
-            }
-            if (++resolved === len) {
-              resolve(res)
-            }
-          },
-          reason => {
-            res[i] = {
-              status: REJECTED_STATE,
-              reason
-            }
-            if (++resolved === len) {
-              resolve(res)
-            }
-          }
-        )
-      }
-    })
   }
 }
 
